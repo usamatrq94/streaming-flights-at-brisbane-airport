f15cfecb11374e8805decdabbeb115fd18c3eefc
muhammad tariq <usama.bt94@gmail.com>
2023-08-03 08:17:32 +1000

    UPDATED: Jenkins to include image build

--- ci/build_and_push_image.sh
+++ ci/build_and_push_image.sh
@@ -4,8 +4,8 @@
 
 gcloud auth configure-docker $REGION-docker.pkg.dev --quiet
 
-sudo docker build -f orchestration/Dockerfile -t $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/execution:latest .
+docker build -f orchestration/Dockerfile -t $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/execution:latest .
 
-sudo docker push $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/execution:latest
+docker push $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/execution:latest
 
 
